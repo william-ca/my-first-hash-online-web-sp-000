f4c49369a23e13ebeca802cbe0cc73c2b32890bc
Will Andre <wandre94@gmail.com>
2021-01-04 15:54:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,21 +1,21 @@
 def my_hash
   my_hash = {"my_name" => "Will", "my_age" => "26", "my_height" => "5ft 9in"}
 
-  
+
 end
 
 
 def shipping_manifest
   the_manifest = {"wale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 
 end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
   shipping_manifest[oil paintings]
@@ -25,8 +25,8 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
