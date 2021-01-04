6e82db982dd6529c69d383cbc5d3fdd23323904b
Will Andre <wandre94@gmail.com>
2021-01-04 15:57:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,7 +6,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"wale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 
